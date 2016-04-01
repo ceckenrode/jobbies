79fdbdcd8de6d854a9b9e7c1ee5ae4c10ea836e4
ceckenrode <chriseckenrode@me.com>
2016-04-01 19:12:49 -0400

    removed gruntfile

--- public/js/controllers/registerController.js
+++ public/js/controllers/registerController.js
@@ -1,21 +1,27 @@
 angular.module("jobbiesApp").controller('RegisterController', ['$scope', '$http', function($scope, $http) {
 
-  $scope.register = function() {
-    $http.post('/register', { email: $scope.email, password: $scope.password })
-      .then(function successCallback(response) {
-        console.log("success");
-        console.log(response.data);
-        $scope.email = '';
-        $scope.password = '';
-      }, function errorCallback(response) {
-        console.log('failed');
-        $scope.email = '';
-        $scope.password = '';
-      });
+  $scope.register = function () {
+        $http.post('/register', {
+            email: $scope.email,
+            password: $scope.password
+        })
+            .then(function successCallback(response) {
+                console.log("success");
+                console.log(response.data);
+                $scope.email = '';
+                $scope.password = '';
+            }, function errorCallback(response) {
+            console.log('failed');
+            $scope.email = '';
+            $scope.password = '';
+          });
   }
 
-  $scope.login = function() {
-    $http.post('/login', { email: $scope.email, password: $scope.password })
+  $scope.login = function () {
+    $http.post('/login', {
+        email: $scope.email,
+        password: $scope.password
+      })
       .then(function successCallback(response) {
         console.log("success");
         console.log(response.data);
