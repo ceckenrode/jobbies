4b9fbdbbcdfcbfbdc9648a3675d7ba25cf7b568c
ceckenrode <chriseckenrode@me.com>
2016-04-02 11:49:12 -0400

    added mongdb url

--- config/db.js
+++ config/db.js
@@ -1,3 +1,4 @@
 module.exports = {
-        url : 'mongodb://localhost/jobbiesApp'
+        // url : 'mongodb://localhost/jobbiesApp'
+        url: 'mongodb://heroku_0lvz1t14:753rasram66asbvso2h565k7n8@ds027295.mlab.com:27295/heroku_0lvz1t14'
     };
