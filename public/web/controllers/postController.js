a5636add6cca71e64b6dfcd52a10d694f025ad87
jmona789 <jmona789@gmail.com>
2016-04-09 11:46:59 -0400

    trying to push emploer id to DB

--- public/web/controllers/postController.js
+++ public/web/controllers/postController.js
@@ -1,6 +1,7 @@
-angular.module("jobbiesApp").controller('PostController', ['$scope', '$http', function($scope, $http){
+angular.module("jobbiesApp").controller('PostController', ['$scope', '$http', '$localStorage', function($scope, $http, $localStorage){
+  console.log($localStorage.user);
   $scope.submitJobbie = function(){
-    $http.post('/api/postjobbie', {title: $scope.jobbieTitle, compensation: $scope.jobbieCompensation, estCompletionTime: $scope.jobbieEstCompletionTime, location: [{address1: $scope.jobbieAddress1, address2: $scope.jobbieAddress2, city: $scope.jobbieCity, state: $scope.jobbieState, zip: $scope.jobbieZip}], description: $scope.jobbieDescription})
+    $http.post('/api/postjobbie', {title: $scope.jobbieTitle, compensation: $scope.jobbieCompensation, estCompletionTime: $scope.jobbieEstCompletionTime, location: [{address1: $scope.jobbieAddress1, address2: $scope.jobbieAddress2, city: $scope.jobbieCity, state: $scope.jobbieState, zip: $scope.jobbieZip}], employer: ["ObjectId('" + $localStorage.user._id + "')"], description: $scope.jobbieDescription})
     .then(function successCallback(response){
       $scope.jobbieTitle = '';
       $scope.jobbieDescription = '';
