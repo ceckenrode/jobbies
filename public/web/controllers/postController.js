5bc792baa510c3bf1ac963a222962188a7b736f2
ceckenrode <chriseckenrode@me.com>
2016-04-10 11:12:10 -0400

    made postcontroller cleaner

--- public/web/controllers/postController.js
+++ public/web/controllers/postController.js
@@ -1,17 +1,15 @@
 angular.module("jobbiesApp").controller('PostController', ['$scope', '$http', '$localStorage', function($scope, $http, $localStorage){
   console.log($localStorage.user);
+
+  $scope.postInit = function(){
+    $scope.newJobbie = {};
+    $scope.newJobbie.location = [{}];
+  }
   $scope.submitJobbie = function(){
-    $http.post('/api/postjobbie', {title: $scope.jobbieTitle, compensation: $scope.jobbieCompensation, estCompletionTime: $scope.jobbieEstCompletionTime, location: [{address1: $scope.jobbieAddress1, address2: $scope.jobbieAddress2, city: $scope.jobbieCity, state: $scope.jobbieState, zip: $scope.jobbieZip}], employer: ["ObjectId('" + $localStorage.user._id + "')"], description: $scope.jobbieDescription})
+    console.log($scope.newJobbie);
+    $http.post('/api/postjobbie', $scope.newJobbie)
     .then(function successCallback(response){
-      $scope.jobbieTitle = '';
-      $scope.jobbieDescription = '';
-      $scope.jobbieCompensation = '';
-      $scope.jobbieEstCompletionTime = '';
-      $scope.jobbieAddress1 = '';
-      $scope.jobbieAddress2 = '';
-      $scope.jobbieCity = '';
-      $scope.jobbieState = '';
-      $scope.jobbieZip = '';
+      $scope.newJobbie = {};
     }, function errorCallback(response){
       alert("SOMETHING WENT WRONG");
     });
